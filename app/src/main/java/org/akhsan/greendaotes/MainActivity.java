750434ab8d4dc446e8a67165efd9d9772ac240ad
Nafis Akhsan <northgear12167@gmail.com>
2018-08-14 02:39:15 +0700

    Add repository to main activity

--- app/src/main/java/org/akhsan/greendaotes/MainActivity.java
+++ app/src/main/java/org/akhsan/greendaotes/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Repository repository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        repository=new Repository();
     }
 }
